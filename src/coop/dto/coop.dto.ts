a5a53a1a41ace19d17cfdb31e55c3e21d357001d
sw347 <>
2024-06-01 18:34:54 +0900

    feat: coop dto

--- src/coop/dto/coop.dto.ts
+++ src/coop/dto/coop.dto.ts
@@ -1,1 +1,33 @@
-export class CoopDto {}
+import { IsUrl } from "class-validator";
+import { Exclude, Expose } from "class-transformer";
+
+@Exclude()
+export class CoopDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  name: string;
+
+  @Expose()
+  location: string;
+
+  @Expose()
+  realLocation?: string;
+
+  @Expose()
+  description: string;
+
+  @Expose()
+  phone: string;
+
+  @Expose()
+  baseUrl: string;
+
+  @Expose()
+  mainImage: string;
+
+  @Expose()
+  subImages: string[];
+
+}
